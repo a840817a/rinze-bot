760fe42576ba18ec1fcef03eddfceef2674bdb13
浩浩 <a840817a@yahoo.com.tw>
2020-10-09 21:51:34 +0800

    first commit

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,7 @@
 FROM node:12-alpine
 ENV NODE_ENV production
 WORKDIR /usr/src/app
-COPY ["package.json", "yarn.lock"] .
+COPY ["package.json", "yarn.lock", "./"]
 RUN yarn install && yarn cache clean
 COPY . .
 CMD yarn start
--- index.js
+++ index.js
@@ -1,25 +1,33 @@
 const Discord = require('discord.js');
+require('dotenv').config()
+
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on('message', msg => {
-  if (msg.content.startsWith('04 D')) {
-    var out = '';
-    msg.content.substr(4).split("D").forEach(element => {
-      var dice = getRandomInt(element);
-      if (isNaN(dice) | msg.content.substr(4) < 2) dice = 'O^O';
-      out = out.concat(dice, ' ');
-    })
-    msg.reply(out)
-    .catch(console.error);
-  }
+    if (msg.content.startsWith('04 D')) {
+        let out = '';
+        msg.content.substr(4).split("D").forEach(element => {
+            let dice = getRandomInt(element);
+            if (isNaN(dice) || msg.content.substr(4) < 2) dice = 'O^O';
+            out = out.concat(dice, ' ');
+        })
+        msg.reply(out)
+            .catch(console.error);
+    }
 });
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max)) + 1;
-  }
+}
 
-client.login(process.env.DISCORD_BOT_TOKEN);
+client.login(process.env.DISCORD_BOT_TOKEN).then(() => {
+    console.log(`Discord bot Rinze Started!`);
+})
+    .catch(() => {
+        console.log(`Unavailable to start bot, check your DISCORD_BOT_TOKEN`);
+        process.exit(1);
+    });
